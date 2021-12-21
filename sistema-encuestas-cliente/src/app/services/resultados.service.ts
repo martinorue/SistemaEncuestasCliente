40f599e3a8f9354800f9d753d2456c4014e68f5e
martinorue <martin.orue008@gmail.com>
2021-12-21 12:21:37 -0300

    borro imports inncesarios

--- sistema-encuestas-cliente/src/app/services/resultados.service.ts
+++ sistema-encuestas-cliente/src/app/services/resultados.service.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { filter, map } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { ResultadoEncuesta } from '../domain/resultadoEncuesta';
 
