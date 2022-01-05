db0e81fad1f251fa788120362da7cde0edca5f1f
martinorue <martin.orue008@gmail.com>
2022-01-05 14:42:44 -0300

    modifico guard para que devuelva true y poder loguearme

--- sistema-encuestas-cliente/src/app/guards/encuesta.guard.ts
+++ sistema-encuestas-cliente/src/app/guards/encuesta.guard.ts
@@ -10,7 +10,7 @@ export class EncuestaGuard implements CanActivate {
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
       console.log('**CanActivate**')
-    return false;
+    return true;
   }
 
 }
