a21a1b0a85df114661f1ebf9268fd3bed30754a8
martinorue <martin.orue008@gmail.com>
2022-01-18 13:05:42 -0300

    agrego attr GET_DASHBOARD para configurar futuro interceptor del dashboard

--- sistema-encuestas-cliente/src/app/commons/static/path-rest.ts
+++ sistema-encuestas-cliente/src/app/commons/static/path-rest.ts
@@ -1,4 +1,5 @@
 import { environment } from './../../../environments/environment';
 export class PathRest {
 	static readonly GET_LOGIN = environment.baseUri + '/login';
+	static readonly GET_DASHBOARD = environment.baseUri + '/dashboard';
 }
