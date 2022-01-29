10ffaa1f6e131bb83de27a0a6e4ca67a73e80d79
martinorue <martin.orue008@gmail.com>
2022-01-29 16:16:20 -0300

    mejora formulario agregar preguntas. Valida que se deban agregar dos opciones por lo menos

--- sistema-encuestas-cliente/src/app/commons/components/agregar-pregunta/agregar-pregunta.component.ts
+++ sistema-encuestas-cliente/src/app/commons/components/agregar-pregunta/agregar-pregunta.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { IOpcion, IPregunta } from '../../../domain/pregunta';
 import { ENTER } from '@angular/cdk/keycodes';
 import { MatChipInputEvent } from '@angular/material/chips';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { TiposPregunta } from 'src/app/domain/tiposPregunta';
 
 @Component({
   selector: 'app-agregar-pregunta',
@@ -11,8 +13,8 @@ import { MatChipInputEvent } from '@angular/material/chips';
 export class AgregarPreguntaComponent implements OnInit {
 
   @Output() preguntaEmitida = new EventEmitter<IPregunta>();
+  tiposPregunta = TiposPregunta;
   pregunta: string = '';
-  tipoPregunta: string = '';
   nuevasPreguntas: IPregunta[] = [];
   requerida: boolean = true;
   orden: number = 1;
@@ -20,30 +22,35 @@ export class AgregarPreguntaComponent implements OnInit {
   opciones: IOpcion[] = [];
   addOnBlur = true;
   readonly separatorKeysCodes = [ENTER] as const;
+  agregarPreguntaForm!: FormGroup;
 
-  constructor() { }
+  constructor(private _fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.crearFormulario();
   }
 
-  tiposPregunta: any = [
-    { value: 'TEXTOLIBRE', viewValue: 'Texto Libre' },
-    { value: 'OPCIONSIMPLE', viewValue: 'Opción Simple' },
-    { value: 'OPCIONMULTIPLE', viewValue: 'Opción Múltiple' },
-  ];
+  crearFormulario() {
+    this.agregarPreguntaForm = this._fb.group({
+      pregunta: ['', [Validators.required]],
+      tiposPregunta: ['', [Validators.required]],
+      requerida: [this.requerida],
+      opciones: [this.opciones, [Validators.required]]
+    });
+  }
 
-  onChange() {
+  onChangeRequerida() {
     !this.requerida;
   }
 
-  agregarNuevaPregunta(value: string) {
+  agregarNuevaPregunta() {
     const nuevaPregunta: IPregunta = {
       PreguntaID: 0,
-      TextoPregunta: value,
-      Tipo: this.tipoPregunta,
+      TextoPregunta: this.agregarPreguntaForm.value.pregunta,
+      Tipo: this.agregarPreguntaForm.value.tiposPregunta,
       Orden: this.orden++,
       EncuestaID: 0,
-      Requerida: this.requerida,
+      Requerida: this.agregarPreguntaForm.value.requerida,
       Opciones: this.opciones,
       Resultados: null,
       ResultadosML: null
@@ -52,29 +59,30 @@ export class AgregarPreguntaComponent implements OnInit {
     this.preguntaEmitida.emit(nuevaPregunta);
     this.pregunta = '';
     this.opciones = [];
-    this.tipoPregunta = '';
+    // this.tipoPregunta = '';
   }
 
   validarPregunta(): boolean {
-    let ok = false;
-    if (this.pregunta != null) {
-      ok = true;
-    }
-    if (this.tipoPregunta == '') {
-      ok = false;
-    }
-    if (this.tipoPregunta == 'OPCIONSIMPLE'
-      && (this.opciones == null || this.opciones.length == 0)) {
-      ok = false;
-    }
-    if (this.tipoPregunta == 'OPCIONMULTIPLE'
-      && (this.opciones == null || this.opciones.length == 0)) {
-      ok = false;
-    }
-    if (this.pregunta.trim() == '') {
-      ok = false;
-    }
-    return ok;
+    // let ok = false;
+    // if (this.pregunta != null) {
+    //   ok = true;
+    // }
+    // if (this.tipoPregunta == '') {
+    //   ok = false;
+    // }
+    // if (this.tipoPregunta == 'OPCIONSIMPLE'
+    //   && (this.opciones == null || this.opciones.length == 0)) {
+    //   ok = false;
+    // }
+    // if (this.tipoPregunta == 'OPCIONMULTIPLE'
+    //   && (this.opciones == null || this.opciones.length == 0)) {
+    //   ok = false;
+    // }
+    // if (this.pregunta.trim() == '') {
+    //   ok = false;
+    // }
+    // return ok;
+    return true;
   }
 
   borrarPregunta(pregunta: IPregunta) {
@@ -88,7 +96,7 @@ export class AgregarPreguntaComponent implements OnInit {
     const value = (event.value || '').trim();
 
     if (value) {
-      this.opciones.push({OpcionID: 0, OpcionTexto: value });
+      this.opciones.push({ OpcionID: 0, OpcionTexto: value });
     }
 
     event.chipInput!.clear();
