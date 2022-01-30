8811618595936c50776e021f6dbfe558235e55c4
martinorue <martin.orue008@gmail.com>
2022-01-30 10:53:03 -0300

    publicar encuesta

--- sistema-encuestas-cliente/src/app/commons/components/encuesta/encuesta.component.ts
+++ sistema-encuestas-cliente/src/app/commons/components/encuesta/encuesta.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CrearEncuestaService } from 'src/app/services/crear-encuesta.service';
 import { IEncuesta } from '../../../domain/encuesta';
 import { EncuestasService } from '../../../services/encuestas.service';
 
@@ -9,13 +10,22 @@ import { EncuestasService } from '../../../services/encuestas.service';
 })
 export class EncuestaComponent implements OnInit {
   encuestas!: IEncuesta[];
+  errMess!: string;
 
   constructor(
-    private servicioEncuestas: EncuestasService
-    ) { }
+    private servicioEncuestas: EncuestasService,
+    private _crearEncuestaService: CrearEncuestaService,
+  ) { }
 
   ngOnInit(): void {
     this.servicioEncuestas.getEncuestas().subscribe(encuestas => this.encuestas = encuestas);
+  }
+
+  publicarEncuesta(encuesta: IEncuesta) {
+    encuesta.Estado = 'PUBLICADA';
+    this._crearEncuestaService.submitEncuestaEditada(encuesta)
+      .subscribe(respuestaSubmit => encuesta = respuestaSubmit,
+        errmess => this.errMess = <any>errmess);
   }
 
 }
