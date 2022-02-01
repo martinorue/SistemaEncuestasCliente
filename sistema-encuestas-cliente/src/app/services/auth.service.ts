f79a3b4f0c2a0520a1129199a72ea530aabfe858
martinorue <martin.orue008@gmail.com>
2022-02-01 10:14:59 -0300

    borrado de console.log

--- sistema-encuestas-cliente/src/app/services/auth.service.ts
+++ sistema-encuestas-cliente/src/app/services/auth.service.ts
@@ -15,19 +15,19 @@ export class AuthService {
   constructor(private http: HttpClient, private processHttpmsgService: ProcessHttpmsgService) { }
   
   token = localStorage.getItem('access_token')!;
+
   login(login: IRQLogin): Observable<HttpResponse<string>> {
     return this.http.post<string>(`${environment.baseUri}/api/login`, login, { observe: 'response' }).pipe(catchError((error) => this.processHttpmsgService.handleError(error)));
   }
   
   registro(usuario: IRegister): Observable<IRegister> {
-    console.log('entré al servicio');
     const httpOptions = {
       headers: new HttpHeaders({
         'Content-Type': 'application/json',
         'Authorization': `Bearer ${this.token}`
       })
     };
-    return this.http.post<IRegister>(`${environment.baseUri}/api/Cuentas/registrar/`, usuario, httpOptions)
+    return this.http.post<IRegister>(`${environment.baseUri}/api/Cuentas/registrar`, usuario, httpOptions)
     .pipe(catchError((error) => this.processHttpmsgService.handleError(error)));
   }
 
